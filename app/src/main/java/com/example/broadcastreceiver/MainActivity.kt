1574b074ba0a206906b6fffa3692c18f49d09777
zvrbsky <piotrzarebski94@gmail.com>
2020-12-06 12:26:48 +0100

    Added posibility to open Shopping List App via notification

--- app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
@@ -2,16 +2,14 @@ package com.example.broadcastreceiver
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.app.PendingIntent
+import android.content.*
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+
 
 const val PRODUCT_NAME = "name"
 
@@ -26,10 +24,16 @@ class MainActivity : AppCompatActivity() {
         createNotificationChannel()
         var id = 0
 
+        val intent = Intent()
+        intent.component = ComponentName("com.example.mini_project", "com.example.mini_project..productList.ProductListActivity")
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
         val notificationBuilder = NotificationCompat.Builder(this,
                 getString(R.string.channelID))
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(getString(R.string.notification_title))
+                .setContentIntent(pendingIntent)
                 .setAutoCancel(true)
 
         val notificationManager = NotificationManagerCompat.from(this)
