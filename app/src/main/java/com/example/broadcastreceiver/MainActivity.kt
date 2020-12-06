cbaeaea298f98b135fbfb75a5b18182ab5a2956f
zvrbsky <piotrzarebski94@gmail.com>
2020-12-06 12:12:19 +0100

    added product name to notification + minor fixes

--- app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
@@ -13,34 +13,37 @@ import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
+const val PRODUCT_NAME = "name"
+
 class MainActivity : AppCompatActivity() {
     lateinit var receiver: BroadcastReceiver
+    private val NEW_PRODUCT_ADDED_BROADCAST = "SHOPPING_LIST_APP.NEW_PRODUCT_ADDED_BROADCAST"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         createNotificationChannel()
-        val requestCode = 1
         var id = 0
 
-        val notification = NotificationCompat.Builder(this,
+        val notificationBuilder = NotificationCompat.Builder(this,
                 getString(R.string.channelID))
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(getString(R.string.notification_title))
-                .setContentText("placeholder")
                 .setAutoCancel(true)
-                .build()
 
         val notificationManager = NotificationManagerCompat.from(this)
 
         receiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context?, intent: Intent?) {
+                val notification = notificationBuilder
+                        .setContentText(intent?.getStringExtra(PRODUCT_NAME))
+                        .build()
                 notificationManager.notify(id++, notification)
             }
         }
         registerReceiver(receiver,
-                IntentFilter("NEW_PRODUCT_ADDED_BROADCAST"))
+                IntentFilter(NEW_PRODUCT_ADDED_BROADCAST))
     }
 
     override fun onDestroy() {
