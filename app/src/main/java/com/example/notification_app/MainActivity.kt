0481bfdbf8affcf5dd9ef0516ebae6b27eb55b9a
zvrbsky <piotrzarebski94@gmail.com>
2020-12-06 15:30:42 +0100

    added buttons for turning on and off notifications

--- app/src/main/java/com/example/notification_app/MainActivity.kt
+++ app/src/main/java/com/example/notification_app/MainActivity.kt
@@ -1,48 +1,27 @@
 package com.example.notification_app
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
+
 import android.content.*
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-
-
-
 
 class MainActivity : AppCompatActivity() {
-    lateinit var receiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
+    fun turnOnNotificationOnClick(view: View) {
         Intent(this, NotificationService::class.java).also { intent ->
             startService(intent)
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(receiver)
-    }
-
-    private fun createNotificationChannel(){
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.O)
-            return
-
-        val notificationChannel = NotificationChannel(
-                getString(R.string.channelID),
-                getString(R.string.channel_name),
-                NotificationManager.IMPORTANCE_DEFAULT
-        )
-        notificationChannel.description =
-                getString(R.string.channel_description)
-        val notificationManager = NotificationManagerCompat.from(this)
-        notificationManager.createNotificationChannel(notificationChannel)
+    fun turnOffNotificationOnClick(view: View) {
+        Intent(this, NotificationService::class.java).also { intent ->
+            stopService(intent)
+        }
     }
 }
